f7ca0020ad5ca7f89660f09a9cfde207f7668993
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-12-10 17:53:52 +0100

    Fehlgeschlagen

--- src/com/company/Book_Instanz.java
+++ src/com/company/Book_Instanz.java
@@ -8,5 +8,11 @@ public class Book_Instanz {
         Book first = new Book(223, new Date(), "My Book", "1234564");
 
         System.out.println("Das Buch hat " + first.getPages() + " Seiten, wurde am " + first.getRel() + " veröffentlicht und hat den Titel " + first.getTitle());
+    
+       /* int[] fav = new int[0];
+        for (:
+             ) {
+       */
+        }
     }
-}
+
---
+++ src/com/company/Paperbook.java
@@ -0,0 +1,23 @@
+/*package com.company;
+
+public class Paperbook extends Book {
+    String series;
+    String month;
+
+    public void setSeries(String series){
+        this.series = series;
+    }
+
+    public void setMonth(String month){
+        this.month = month;
+    }
+
+    public String getSeries(){
+        return series;
+    }
+
+    public String getMonth(){
+        return month;
+    }
+}
+*/
