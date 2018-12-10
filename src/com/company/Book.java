a280f09ca77caef05928d22a6fddf23167f6db6a
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-12-10 17:39:40 +0100

    instanz book

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,4 +1,53 @@
 package com.company;
 
+import java.util.Date;
+
 public class Book {
+
+    int pages;
+    Date rel;
+    String title;
+    String isbn;
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setRel(Date rel){
+        this.rel = rel;
+    }
+
+
+    public int getPages(){
+        return pages;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getIsbn(){
+        return isbn;
+    }
+
+    public Date getRel(){
+        return rel;
+    }
+
+
+    public Book(int pages, Date rel, String title, String isbn)
+    {
+        this.pages = pages;
+        this.rel = rel;
+        this.title = title;
+        this.isbn = isbn;
+    }
 }
---
+++ src/com/company/Book_Instanz.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Date;
+
+public class Book_Instanz {
+    public static void main(String[] args) {
+
+        Book first = new Book(223, new Date(), "My Book", "1234564");
+
+        System.out.println("Das Buch hat " + first.getPages() + " Seiten, wurde am " + first.getRel() + " veröffentlicht und hat den Titel " + first.getTitle());
+    }
+}
